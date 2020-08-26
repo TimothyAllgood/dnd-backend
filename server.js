119840b45626add910e2590ac239ab03c84313ba
Timothy Allgood <timothyallgood@gmail.com>
2020-08-25 19:00:10 -0500

    added RTC with socket io

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
 require('dotenv').config();
 
 const routes = require('./routes');
@@ -14,8 +17,7 @@ app.use(
 	cors({
 		origin: [`http://localhost:3000`],
 		methods: 'GET,POST,PUT,DELETE',
-		// credentials: true, // allows the session cookie to be sent back and forth from server to client
-		optionsSuccessStatus: 200, // some legacy browsers choke on satus 204
+		optionsSuccessStatus: 200,
 	})
 );
 
@@ -23,10 +25,23 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+io.sockets.on('connection', (socket) => {
+	socket.on('fromClient', (data) => {});
+
+	const room = socket.request.headers.referer.split('/').pop();
+
+	socket.join(room);
+
+	socket.on('SEND_MESSAGE', function (data) {
+		console.log('message sent');
+		io.to(data.to).to(data.from).emit('RECEIVE_MESSAGE', data);
+	});
+});
+
 // Auth Routes
 app.use('/api/v1/auth', routes.auth);
 app.use('/api/v1/users', routes.user);
 app.use('/api/v1/messages', routes.messages);
 
 // Start Server
-app.listen(PORT, console.log('Server running on port ', PORT));
+server.listen(PORT, console.log('Server running on port ', PORT));
